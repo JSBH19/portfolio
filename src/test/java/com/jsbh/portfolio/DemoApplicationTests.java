7263d968ec6e7e7112cdc162a12d1051a1e964ea
dopamine100 <dopamine100@dopamine100-2.local>
2019-09-21 18:38:40 +0900

    html test

--- src/test/java/com/jsbh/portfolio/DemoApplicationTests.java
+++ src/test/java/com/jsbh/portfolio/DemoApplicationTests.java
@@ -1,7 +1,13 @@
 package com.jsbh.portfolio;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +15,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@Autowired
+	private DataSource ds;
+
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void connectionTest() throws SQLException {
+
+		System.out.println(" >_ < ");
+		System.out.println(ds);
+		Connection con = ds.getConnection();
+		System.out.println(con);
+	}
 }
